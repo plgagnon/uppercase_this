35914d1115683396f35040a8f0f5b559a5bb5379
plgagnon <pierrelouis.gagnon@gmail.com>
2025-08-13 12:03:49 -0400

    App completed

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
-st.title("Hello, Streamlit!")
+def uppercase_this():
+    st.session_state['this_string'] = st.session_state['this_string'].upper()
+
+
+st.text_input(
+    "Enter a string:",
+    key='this_string',
+)
+
+st.button(
+    label="Uppercase the string above",
+    key='uppercase_button',
+    on_click=uppercase_this
+)
+
+# st.write(f"Current string: {st.session_state.get('this_string', '')}")
